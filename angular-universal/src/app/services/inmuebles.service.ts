73c99c870dc340647003e18fb4bff4a1af6f233d
Nicolas Hernandez Siachoque <n.hernandezs@uniandes.edu.co>
2018-07-14 21:42:15 -0500

    Permite mirar la lista de inmuebels tanto con la realtime database como con la cloud fire

--- angular-universal/src/app/services/inmuebles.service.ts
+++ angular-universal/src/app/services/inmuebles.service.ts
@@ -43,17 +43,27 @@ export class InmueblesService {
         this.inmueblesRTD= dataBase.list('/posts');
 
         
-
-        // Actualiza la lista de posts
-        this.posts=this.inmueblesCF.snapshotChanges().map(actions=>{
-            return actions.map(item=>{
-                const data=item.payload.doc.data() as IInmueble;
-                const id =item.payload.doc.id;
+        //FAVOR NO BORRAR
+        // Actualiza la lista de posts con el cloud fire
+        // this.posts=this.inmueblesCF.snapshotChanges().map(actions=>{
+        //     return actions.map(item=>{
+        //         const data=item.payload.doc.data() as IInmueble;
+        //         const id =item.payload.doc.id;
             
-                //Une al data y al id
+        //         //Une al data y al id
+        //         return {...data,id};
+        //     });
+        // })
+
+        //Actualiza la lista de posts con el realtime database
+        this.posts=this.inmueblesRTD.snapshotChanges().map(actions=>{
+            return actions.map(item=>{
+                const data=item.payload.val() as IInmueble;
+                const id =item.payload.key;
                 return {...data,id};
             });
         })
+
      }
 
      /**
