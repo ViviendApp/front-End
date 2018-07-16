dc5f65f87378552e00018d52ec74e39c9c925946
Camilo Andres Salinas Martinez <camilo@ggitapps.com>
2018-07-16 18:10:28 -0500

    hecho 404

--- angular-universal/src/app/error-not-found/error-not-found.component.ts
+++ angular-universal/src/app/error-not-found/error-not-found.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-error-not-found',
   templateUrl: './error-not-found.component.html',
-  styles: []
+  styleUrls: ['./error-not-found.component.css']
 })
 export class ErrorNotFoundComponent implements OnInit {
 
