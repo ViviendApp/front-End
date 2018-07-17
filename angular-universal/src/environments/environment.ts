50ad6a0c7c5fd3d79f3763e8b80ab629ee9d5ff5
Camilo Andres Salinas Martinez <camilo@ggitapps.com>
2018-07-17 18:54:29 -0500

    Cambios Fail para prod

--- angular-universal/src/environments/environment.ts
+++ angular-universal/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   production: false,
   firebase:{
     apiKey: "AIzaSyDUOWIa9_uM2ez4inGji_rM4jcQh9T7Q3A",
-    authDomain: "viviendapp-c3907.firebaseapp.com",
+    authDomain: "auth.viviendapp.co",
     databaseURL: "https://viviendapp-c3907.firebaseio.com",
     projectId: "viviendapp-c3907",
     storageBucket: "viviendapp-c3907.appspot.com",
