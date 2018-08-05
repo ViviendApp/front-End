55fced049f24e7a5d313685a905ddf0f0a41901d
Nicolas Hernandez Siachoque <n.hernandezs@uniandes.edu.co>
2018-08-04 23:54:37 -0500

    Permite subir mas de una foto al publicar y guarda correctamente las imagenes, ERROR en mostrar el progreso de subida

--- angular-universal/src/app/nuevo-inmueble/drop-zone/drop-zone.component.ts
+++ angular-universal/src/app/nuevo-inmueble/drop-zone/drop-zone.component.ts
@@ -41,36 +41,48 @@ export class DropZoneComponent {
 
         startUpload(event:FileList){
             // The File object
-            const file = event.item(0)
+            
+            console.log(event.length);
 
-            // Client-side validation example
-            if (file.type.split('/')[0] !== 'image') { 
-            console.error('unsupported file type :( ')
-            return;
+            for (let index = 0; index < event.length; index++) {
+                const file = event.item(index);
+                
+                // Client-side validation example
+                if (file.type.split('/')[0] !== 'image') { 
+                console.error('unsupported file type :( ')
+                return;
+                }
+                
+                // The storage path
+                const path = `posts/${this.idInmueble}/${new Date().getTime()}_${file.name}`;
+                
+                // Totally optional metadata
+                const customMetadata = { app: 'My AngularFire-powered PWA!' };
+    
+                // Inmuebles service upload the image
+                this.task=this.inmueblesS.startUploadImg(event,path,file,customMetadata);
+                
+                // Progress monitoring
+                //El procentaje es el % de fotos que lleva mas el porcentaje de lo que va de la foto actual
+                //lo que lleva 
+                var lleva= index/event.length;
+                this.percentage = this.task.percentageChanges();
+                this.snapshot   = this.task.snapshotChanges();
+           
+                // The file's download URL
+                this.snapshot.pipe(finalize(() => {this.downloadURL = this.storage.ref(path).getDownloadURL()
+                    
+                    this.downloadURL.subscribe(val => {console.log(77777);console.log(val); 
+                    
+                        //Manda el evento de que se subio una foto
+                        this.rutaFoto.emit(val);
+                        });
+                })).subscribe();
             }
 
-            // The storage path
-            const path = `posts/${this.idInmueble}/${new Date().getTime()}_${file.name}`;
-            
-            // Totally optional metadata
-            const customMetadata = { app: 'My AngularFire-powered PWA!' };
 
-            // Inmuebles service upload the image
-            this.task=this.inmueblesS.startUploadImg(event,path,file,customMetadata);
 
-            // Progress monitoring
-            this.percentage = this.task.percentageChanges();
-            this.snapshot   = this.task.snapshotChanges()
 
-            // The file's download URL
-            this.snapshot.pipe(finalize(() => {this.downloadURL = this.storage.ref(path).getDownloadURL()
-                
-                this.downloadURL.subscribe(val => {console.log(77777);console.log(val); 
-                
-                    //Manda el evento de que se subio una foto
-                    this.rutaFoto.emit(val);
-                    });
-            })).subscribe();
         }
          // Determines if the upload task is active
             isActive(snapshot) {
