22b8df38ad29bd8ecc12c1e23f65f8694e7f1774
Nicolas Hernandez Siachoque <n.hernandezs@uniandes.edu.co>
2018-08-05 00:09:39 -0500

    Para tener una guia del prgreso puse el numero de fotos subidas

--- angular-universal/src/app/nuevo-inmueble/drop-zone/drop-zone.component.ts
+++ angular-universal/src/app/nuevo-inmueble/drop-zone/drop-zone.component.ts
@@ -33,6 +33,12 @@ export class DropZoneComponent {
   //Arreglo de imagenes subidas
   images : string[];
 
+  //Numero de foto actual subiendose
+  actual : number = 1;
+
+  //Total de fotos subiendose
+  total : number = 0;
+
     constructor(private inmueblesS: InmueblesService,private storage: AngularFireStorage){    }
 
         toogleHover(event:boolean){
@@ -41,11 +47,13 @@ export class DropZoneComponent {
 
         startUpload(event:FileList){
             // The File object
-            
+            this.total=event.length;
             console.log(event.length);
+            this.actual=1;
 
             for (let index = 0; index < event.length; index++) {
                 const file = event.item(index);
+                console.log(index);
                 
                 // Client-side validation example
                 if (file.type.split('/')[0] !== 'image') { 
@@ -73,7 +81,11 @@ export class DropZoneComponent {
                 this.snapshot.pipe(finalize(() => {this.downloadURL = this.storage.ref(path).getDownloadURL()
                     
                     this.downloadURL.subscribe(val => {console.log(77777);console.log(val); 
-                    
+                        //Parece machete pero no lo es
+                        //Al iniciar en 1 y no en 0, se pasa del total de fotos
+                        //Se suma al terminar de subir, no mientras sube, por eso no debe sumar despues de acabar con todas las fotos
+                        if(this.actual!=this.total)
+                            this.actual=this.actual+1;
                         //Manda el evento de que se subio una foto
                         this.rutaFoto.emit(val);
                         });
